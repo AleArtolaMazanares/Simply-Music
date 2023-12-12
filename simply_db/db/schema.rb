6ae82ce70b4f72fb69cd2b355c7d9fca68b5ad27
AleArtolaManzanares <aartolam@fwdcostarica.com>
2023-12-11 21:21:32 -0600

    update code rails

--- simply_db/db/schema.rb
+++ simply_db/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_08_202217) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_12_031819) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -88,12 +88,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_08_202217) do
 
   create_table "content_artists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.string "image"
     t.text "description", size: :long
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.binary "image", size: :medium
     t.index ["user_id"], name: "index_content_artists_on_user_id"
   end
 
